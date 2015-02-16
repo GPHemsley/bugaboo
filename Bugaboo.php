54ba04f7d0aafd9074034ce5eab1e05950f0666d
Gordon P. Hemsley <me@gphemsley.org>
2015-02-16 14:15:56 -0500

    Initialize extension.

--- Bugaboo.php
+++ Bugaboo.php
@@ -1,3 +1,13 @@
 <?php
 # vim: set ts=4 sw=4 sts=4 noet
 
+$wgExtensionCredits['parserhook'][] = array(
+	'path' => __FILE__,
+	'name' => 'Bugaboo',
+	'descriptionmsg' => '',
+	'version' => '1.0',
+	'author' => array( 'Gordon P. Hemsley', 'Christie Koehler' ),
+	'url' => 'https://github.com/MozillaWiki/bugaboo',
+	'license-name' => 'MPL 2.0',
+);
+
